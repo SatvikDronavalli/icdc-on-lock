5b77ba609af2c21a65816b6ad9c632a7e2923ec5
SatvikDronavalli <send2satvikd@gmail.com>
2025-04-16 23:06:26 +0200

    Update inventory_regression.py

--- inventory_regression.py
+++ inventory_regression.py
@@ -6,13 +6,11 @@ from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-# Load and prepare training data
 df = pd.read_csv("toyota_camry_sales_400_samples_gas_trend.csv")
 X = df.drop(columns=["Predicted_Sales","Month"])
 y = df["Predicted_Sales"]
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
 
-# Train model
 model = LinearRegression()
 model.fit(X_train, y_train)
 y_preds = model.predict(X)
@@ -24,14 +22,12 @@ y_preds = model.predict(X)
     plt.show() '''
 print("R² on training data:", r2_score(y,y_preds))
 
-# Load and transform test data
 for i in X_test.columns:
     plt.scatter(X_test[i],y_test,color='blue',edgecolor='k',s=80)
     plt.xlabel(i)
     plt.ylabel("Model Predicted Sales")
     plt.title(f"{i} vs Model Predicted Sales")
     plt.show() 
-# Evaluate
 plt.scatter(range(len(y_test)),y_test,color='blue',edgecolor='k',s=80)
 plt.scatter(range(len(y_test)),model.predict(X_test),color='red',edgecolor='k',s=80)
 plt.show()
